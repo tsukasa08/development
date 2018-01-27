03c22af9c39014ad1e57bc4f2e4d49cd84b1f293
tsukasa8 <tsukasa.0705t@gmail.com>
2018-01-27 13:27:38 +0900

    StartWindowクラスを追加

---
+++ source/RegisterMovieTitle/RegisterMovieTitle/StartWindow.cpp
@@ -0,0 +1,11 @@
+#include "StartWindow.h"
+
+
+StartWindow::StartWindow()
+{
+}
+
+
+StartWindow::~StartWindow()
+{
+}
---
+++ source/RegisterMovieTitle/RegisterMovieTitle/StartWindow.h
@@ -0,0 +1,8 @@
+#pragma once
+class StartWindow
+{
+public:
+	StartWindow();
+	~StartWindow();
+};
+
